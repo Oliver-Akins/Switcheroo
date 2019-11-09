97d9fef1f7d3c4b9f95a0900749fb6a097aa5f25
Tyler-A <Tyler.akins1999@gmail.com>
2019-11-09 16:21:00 -0600

    Finish up work on the generator, start work on the move function.

--- src/transfer.py
+++ src/transfer.py
@@ -1,26 +1,57 @@
 import os
 from config import Config
 from pathlib import Path
+from renaming import RENAME
 
 
-def GET_FILES_TO_MOVE():
+def get_files():
     """
         Generates files that have not been moved yet. Each yield is a full file path
     """
+
     config = Config()
-    files = Path(config.SOURCE + "Album/").glob
-    print(files)
+    files = []
+
+    # Collect all the files we need to generate
+    source = Path(config.SOURCE + "Album/").glob(
+        "[0-9][0-9][0-9][0-9]/[0-9][0-9]/[0-9][0-9]/*"
+    )
+
+    # Generate each file
+    for file in source:
+        file_ext = file.split(".")[-1]
+        if config.DO_IMAGES and file_ext in config.IMAGE_FORMATS:
+            yield file
+        elif config.DO_VIDEOS and file_ext in config.VIDEO_FORMATS:
+            yield file
+        else:
+            sys.exit("Oh no, the impossible happened, this shouldn't be happening, if it did, make an issue at https://github.com/Tyler-A/Nintendo-Switch-Manager/issues/new with the error code: 0x5A748F4")
 
 
 
 """
 GET LIST OF FILES (TYLKER)
-RENAME ALL FILES IN THE LIST (RENAME_FILE (only accepts the filename, not full path))
+Check which files we actually have to move
+MOVE FILE (Dminer)
 """
 
-def MOVE_FILE(from_filepath, to_filepath):
+# from_filepath = /.../Nintendo/Album/2019/11/9/date-titleid.png
+#
+# new_filepath = {config.TARGET}/game_name/filename_from_RENAME_FILE.<ext>
+def MOVE_FILE(from_filepath):
     config = Config()
+    new_path = ""
+
+    if(config.KEEP_ON_SD):
+        new_path = Path(config.SOURCE, "Album", "Organized")
+    else:
+        pass
+        # TODO Move to path other than on sd
+
+
+
 
 
 if __name__ == "__main__":
-    GET_FILES_TO_MOVE()
+    from_filepath = "/home/tyler/Desktop/coding/Nintendo-Switch-Photo-Manager/Nintendo/Album/2017/03/03/2017030311400900-F1C11A22FAEE3B82F21B330E1B786A39.jpg"
+    MOVE_FILE(from_filepath)
